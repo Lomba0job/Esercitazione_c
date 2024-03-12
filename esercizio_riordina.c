804324ca9f417b2eeca8fd77485ee77c04315243
RED-ITA <enricosalvagiani@gmail.com>
2024-03-12 19:04:22 +0100

    modifiche termiante

--- esercizio_riordina.c
+++ esercizio_riordina.c
@@ -1,13 +1,13 @@
 /*
-Creare tre array di dimensione variabile in base alla richiesta iniziale. 
-riempire il primo array da tastiera, con numeri casuali 
-riordinare i valori del primo array in ordine crescente salvandoli sul secondo 
+Creare tre array di dimensione variabile in base alla richiesta iniziale.
+riempire il primo array da tastiera, con numeri casuali
+riordinare i valori del primo array in ordine crescente salvandoli sul secondo
 riordinare i valori del primo array in ordine decrescente salvandoli sul terzo
 
 Ottenere un OUTPUT composoto così:
 
 
-Quanti valori si volgiono riordinare: 4
+Quanti valori si volgiono riordinare? 4
 
 inserire il valore n1: 1
 inserire il valore n2: 5
@@ -16,8 +16,8 @@ inserire il valore n4: 4
 
 
 array principale:   1 5 3 4
-array cres cente:    1 3 4 5
-array decrescente:  5 4 3 1 
+array crescente:    1 3 4 5
+array decrescente:  5 4 3 1
 
 */
 
@@ -27,7 +27,7 @@ int main(void)
 {
     int N;
 
-    printf("Di quanti valori vuoi che sia composto il vettore? ");
+    printf("Quanti valori si volgiono riordinare? ");
     scanf("%d", &N);
 
     int v[N], v2[N], v3[N];
@@ -37,57 +37,56 @@ int main(void)
         printf("inserire il valore n%d: ", i+1);
         scanf("%d", &v[i]);
         v2[i] = v[i];
+        v3[i] = v[i];
     }
 
 
     for(int i=0; i<N-1; i++)
     {
         int appoggio;
-        printf("\n\n Esecuzione %d:\n", i+1);
         for(int a=i+1;a<N;a++)
         {
             appoggio = v2[a];
 
             if (appoggio < v2[i])
             {
-                printf("Swap:\t");
                 int temp = v2[i];
                 v2[i] = appoggio;
                 v2[a] = temp;
-                for (int j=0; j<N; j++)
-                {
-                    if(j == i || j == a)
-                    {
-                        printf(" (");
-                    }
-                    else
-                    {
-                        printf(" ");
-                    }
-                    printf("%4d", v2[j]);
-                    if(j == i || j == a)
-                    {
-                        printf(") ");
-                    }
-                    else
-                    {
-                        printf(" ");
-                    }
-                    printf(" - ");
-
-                }
-                printf("\n");
-
-
-
-
             }
+
         }
     }
-    printf("\n\nRisultato FINALE:\n");
+
+    for(int i=0; i<N-1; i++)
+    {
+        int appoggio;
+        for(int a=i+1;a<N;a++)
+        {
+            if (v3[a] > v3[i])
+            {
+                int temp = v3[i];
+                v3[i] = v3[a];
+                v3[a] = temp;
+            }
+
+        }
+    }
+
+    printf("\n\narray principale:\t");
+    for (int i=0; i<N; i++)
+    {
+        printf("%4d ", v[i]);
+    }
+    printf("\narray crescente:\t");
     for (int i=0; i<N; i++)
     {
         printf("%4d ", v2[i]);
+    }
+    printf("\narray decrescente:\t");
+    for (int i=0; i<N; i++)
+    {
+        printf("%4d ", v3[i]);
     }
 
 
