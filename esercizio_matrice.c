0a32308322867c767756ce761c01d0b66e4b3894
Michele Lombardi <michelelombardi.job@gmail.com>
2024-03-12 19:22:35 +0100

    inziato esercizio matrice

---
+++ esercizio_matrice.c
@@ -0,0 +1,19 @@
+/**
+ * @file esercizio_matrice.c
+ * @description 
+ * @author Lombardi Michele 
+ */
+
+
+
+/*
+Scrivere un porgramma in c per effetutuare le seguneti funzinoi:
+
+inserire i valori da tastiera nella prima riga di una matrice composta da 3 riche e 10 colonne 
+inserire altri valori nell seconda riga  della stessa matrice, 
+poi calcolare la somma della prima riga con la seconda riga scrivendo il risultato colonna per colonna nella terza riga
+
+stampare l'intera matrice (CONSIGLIO ciclo for annidato)
+*/
+
+
