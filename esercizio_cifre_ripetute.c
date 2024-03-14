a1741abbd9da16ad7460208c63e6cc5131f72155
Michele Lombardi <michelelombardi.job@gmail.com>
2024-03-14 19:06:35 +0100

    Esercizio Matrice (cifre ripetute)

---
+++ esercizio_cifre_ripetute.c
@@ -0,0 +1,15 @@
+/**
+ * @file esercizio_cifre_ripetute.c
+ * @description 
+ * @author Lombardi Michele 
+ * @copyright Nanolever 
+ */
+
+
+/*
+Salvare da tastiera un numero
+scrivere una matrice composta da 10 colonne e 2 righe
+la prima riga rappresetna le cifre disponibili 0-9 
+la seconda colonna rappresenta per ogni cifra quante volte si ripete all'interno del numero salvato precedentemente
+ 
+*/
