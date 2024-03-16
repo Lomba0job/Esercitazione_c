a3fded4d1139ff7c9977a51d9ca194c17554b689
RED-ITA <enricosalvagiani@gmail.com>
2024-03-16 17:37:49 +0100

    programma iniziato

--- stringhe/esercizio_parole_palindrome.c
+++ stringhe/esercizio_parole_palindrome.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#define DIM 30
 
+int main (void)
+{
+    char parola[DIM];
+
+    printf("Inserisci la parola: ");
+    gets(parola);
+
+
+    //Bisogna confrontare li pirmo con l'ultimo carattere e cosi via fino a metà. 
+    //Se sono uguali tutti i caratteri, allora stampa Palindromo. 
+}
