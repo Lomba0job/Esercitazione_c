c68a11887268c45c06ce5bf11fc5bc015828b59f
RED-ITA <enricosalvagiani@gmail.com>
2024-03-18 19:14:07 +0100

    programma terminato

--- stringhe/esercizio_parole_palindrome.c
+++ stringhe/esercizio_parole_palindrome.c
@@ -1,14 +1,43 @@
+/**
+ * @file esercizio_parole_palindrome.c
+ * @description controlla se una parola è palindroma o non palindroma
+ * @author Enrico Salvagiani 
+ */
+
+/*
+OUTPUT 
+----------------------------------------------------------------------
+Inserisci la parola: itopinonavevanonipoti
+palindroma
+
+*/
+
 #include <stdio.h>
-#define DIM 30
+#include <string.h> //libreria per utilizzare funzioni per le stringhe
+#define DIM 30 //dimensione massima per la parola in input
 
 int main (void)
 {
-    char parola[DIM];
+    char parola[DIM]; //la parola data dall'utente assume DIM come dimensione massima
 
     printf("Inserisci la parola: ");
-    gets(parola);
+    gets(parola); //come lo scanf ma non serve assegnare l'indirizzo di memoria
 
+    int len = strlen(parola); //la variabile len ci permette di capire quanto è lunga la stringa data in input
+    int flag = 0; //utilizzata per l'output finale
+    for(int i=0; i<=len/2; i++) //il ciclo for si ripete fino a metà della lughezza della parola data
+    {
+        if(parola[i]!=parola[len-i-1]) //controllo diversità tra i caratteri iniziali e finali e mi avvicino sempre di più al centro con una manovra a tenaglia
+        {
+            flag = 1; //salva se la condizione sia verificata
+            break; //non appena la condizione si verifica, esce dal for
+        }
+    }
 
-    //Bisogna confrontare li pirmo con l'ultimo carattere e cosi via fino a metà. 
-    //Se sono uguali tutti i caratteri,j allora stampa Palindromo. 
+    if(flag == 0) //se è stato eseguito tutto il for la parola è palindroma
+        printf("palindroma");
+    else //altrimenti no
+        printf("non palindroma");
+
+    return 0;
 }
