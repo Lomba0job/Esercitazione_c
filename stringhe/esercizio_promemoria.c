e8bfd380d50a1a606c7bd1f286ca4d6549dcd308
RED-ITA <enricosalvagiani@gmail.com>
2024-03-22 19:21:17 +0100

    commenti aggiunti£

--- stringhe/esercizio_promemoria.c
+++ stringhe/esercizio_promemoria.c
@@ -12,7 +12,7 @@
 
 int main(void)
 {
-    char reminders[MAX_REMIND][MSG_LEN+1];
+    char reminders[MAX_REMIND][MSG_LEN+1]; // sembra una matrice ma è un array di string la seconda posizione la usiamo per  i caratteri della stringa
     char day_str[3], msg_str[MSG_LEN+1];
     int ch, day, i, j, num_remind = 0;
 
@@ -24,39 +24,40 @@ int main(void)
             break;
         }
         printf("Enter a day reminder: ");
-        scanf("%2d", &day);
+        scanf("%2d", &day); // salvare il giorno 
 
-        if (day == 0)
+        if (day == 0) //se è zero uscire dal loop infinito per stampa risultati 
             break;
-            
-        sprintf(day_str, "%2d", day);
+
+        sprintf(day_str, "%2d", day); // salvare sulla stringa day_str la stringa derivata da day in 2 spazi 
         i=0;
 
-        while ((ch = getchar()) != '\n') //legge i promemoria
+        while ((ch = getchar()) != '\n') //legge i promemoria finchè non c'è un a capo con \n anche gli spazi 
         {
-            if (i < MSG_LEN)
-                msg_str [i++] = ch;
+            if (i < MSG_LEN) // se i ancora sta ancora nella dimensione massima della stringa 
+                msg_str [i++] = ch; // salva il carattere nella relativa posizione 
         }
 
-        msg_str [i] = '\0';
+        msg_str [i] = '\0'; // aggiungi l' end line della stringa 
 
-        for (i = 0; i < num_remind; i++)
+        for (i = 0; i < num_remind; i++) // scorri l'array per quanti numeri sono attualmemte salvati 
         {
-            if (strcmp(day_str, reminders[i]) < 0)
+            if (strcmp(day_str, reminders[i]) < 0) //compara il day string con il reminder nella posizione attuale se è minore esci 
                 break;
         }
-        for (j = num_remind; j > i; j--)
+
+        for (j = num_remind; j > i; j--) // scorri al contrario fino a maggiore di i che è il punto di uscita dal for precedente 
         {
-            strcpy(reminders[j], reminders[j-1]);
+            strcpy(reminders[j], reminders[j-1]); // copia la stringa in posizione attuale con quella nella posizione precedente 
         }
-        strcpy(reminders[i], day_str);
-        strcat(reminders[i], msg_str);
-        num_remind++;
+        strcpy(reminders[i], day_str); // quando a finito ci sarà un buco nell'array dove inseriremo day str 
+        strcat(reminders[i], msg_str); // concateniamo anche il reminder 
+        num_remind++; // incrementiamo il numero di promeria salvati 
     }
 
-    printf("\nDay Reminder\n");
-    for (i = 0; i < num_remind; i++)
-    printf(" %s\n", reminders[i]);
+    printf("\nDay Reminder\n"); // stampa tutto (caso inserito 0 come giorno )
+    for (i = 0; i < num_remind; i++) // scorri l'array fino alla poszione dell unltimo promemoria salvato 
+        printf(" %s\n", reminders[i]); // stampa il relativo promemoria 
     return 0;
 
 
