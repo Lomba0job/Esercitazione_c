b05fb06c70514737132063b4a27620e84fbb2bad
RED-ITA <enricosalvagiani@gmail.com>
2024-03-25 11:53:13 +0100

    esercizio commentato

--- stringhe/esercizio_ordina_stringhe.c
+++ stringhe/esercizio_ordina_stringhe.c
@@ -7,35 +7,35 @@ usare, o due matrici, o una a 3 dimensioni
 */
 
 #include <stdio.h>
-#include <string.h>
+#include <string.h> //la libreria per le funzioni relative alle stringhe
 
-#define ROW 8
-#define MAX_LEN_WORD 10
-#define MAX_LEN_DESC 5000
+#define ROW 8 //numero di righe della matrice
+#define MAX_LEN_WORD 10 //lunghezza massima della parola
+#define MAX_LEN_DESC 5000 //lunghezza massima della descrizione
 
 int main(void)
 {
-    char matrix_parole[ROW][MAX_LEN_WORD];
-    char matrix_descrizioni[ROW][MAX_LEN_DESC+1];
-    int num;
-    int c;
+    char matrix_parole[ROW][MAX_LEN_WORD]; //dichiaro una matrice matrix_parole di righe ROW e di parole lunghe MAX_LEN_WORD
+    char matrix_descrizioni[ROW][MAX_LEN_DESC+1]; //dichiaro una matrice matrix_descrizioni di righe ROW e contenente la descrizione delle parole di MAX_LEN_DESC+1
+    int num; //variabile utilizzata per rintracciare la posizione delle parole nella matrice
+    int c; //fintanto che il carattere non corrisponde a '}' continua ad andare avanti a chiedere caratteri per comporre la descrizione della parola in questione
 
-    for(int i=0; i < ROW; i++)
+    for(int i=0; i < ROW; i++) //ciclo for relativo all'inserimento delle parole e alla loro descrizione
     {
         printf("inserisci la parola: \n");
         //salva la parola
-        scanf("%s", matrix_parole[i]);
+        scanf("%s", matrix_parole[i]); //salva l'array di caratteri che compone la parola nella posizione i corrente
 
-        printf("inserisci la descrizione della parola [termina la descrizione  con }]: \n");
+        printf("inserisci la descrizione della parola (termina la descrizione  con }): \n");
         //salva la descrizione
-        int j = 0;
-        while ((c=getchar()) != '}')
+        int j = 0; //variabile di controllo della lunghezza della descrizione
+        while ((c=getchar()) != '}') //fintanto che il carattere non corrisponde a '}' continua ad andare avanti a chiedere caratteri per comporre la descrizione della parola in questione
         {
-            if(j < MAX_LEN_DESC)
+            if(j < MAX_LEN_DESC) //fintanto che la descrizione non è lunga quanto MAX_LEN_DESC vengono inseriti caratteri nella matrice
                 matrix_descrizioni[i][j++] = c;
 
         }
-        matrix_descrizioni[i][j] = '\0';
+        matrix_descrizioni[i][j] = '\0'; //caratere invio
     }
 
     //ordina per parola (ordina anche la relativa descrizione)
@@ -46,7 +46,7 @@ int main(void)
             if(strcmp(matrix_parole[i],matrix_parole[j]) > 0)
             {
                 // le scambiamo
-                char app[MAX_LEN_WORD];
+                char app[MAX_LEN_WORD]; //variabile d'appoggio necessaria per l'ordinamento
                 strcpy(app, matrix_parole[i]);
                 strcpy(matrix_parole[i], matrix_parole[j]);
                 strcpy(matrix_parole[j], app);
@@ -61,9 +61,9 @@ int main(void)
 
 
 
-    for(;;)
+    for(;;) //ciclo infinito
     {
-        system("cls");
+        system("cls"); //comando che fa in modo che la schermata venga pulita per mostrare il contenuto selezionato più recente
         //stampa tutte le parole con un numero davanti 1-ROW MENU
         printf("\n\n\t--------MENU--------");
         for (int i=0; i<ROW; i++)
