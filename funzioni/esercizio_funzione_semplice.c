785389855a4cd24f8c34c01be881a8534785d8ec
RED-ITA <enricosalvagiani@gmail.com>
2024-03-25 20:03:34 +0100

    esercizio fatto

--- funzioni/esercizio_funzione_semplice.c
+++ funzioni/esercizio_funzione_semplice.c
@@ -1,6 +1,28 @@
 /**
  * @file esercizio_funzione_semplice.c
  * @description Primo eserciizo utilizzando le funzioni
- * @author Lombardi Michele 
+ * @author Lombardi Michele
  */
 
+/*
+Funzione di count down
+*/
+
+#include <stdio.h>
+
+void countdown(int a)
+{
+    for (int i=a; i>=0; i--)
+    {
+        printf("%d\n", i);
+    }
+    return;
+}
+
+int main (void)
+{
+    int b;
+    printf("value: ");
+    scanf("%d", &b);
+    countdown(b);
+}
