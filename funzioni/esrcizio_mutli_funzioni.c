0201eb4f1fb6a28770df5fa0778186af2fbdcea5
RED-ITA <enricosalvagiani@gmail.com>
2024-03-27 19:32:01 +0100

    commenti

--- funzioni/esrcizio_mutli_funzioni.c
+++ funzioni/esrcizio_mutli_funzioni.c
@@ -11,9 +11,10 @@ scrivere le funzioni che:
 7) funzione di stampa di tutto, passando tutti i singoli valori
 */
 #include <stdio.h>
-#define MAX 100
+#define MAX 100 //massimo di valori contenuti nel vettore
 
-//prototipo
+//prototipo anticipazione delle funzioni utilizzate dal programma
+//la loro presenza qui consente di dichiarare funzioni anche dopo il main
 int massimo(int a[], int n);
 int minimo(int a[], int n);
 long int somma(int a[], int n);
@@ -25,18 +26,19 @@ void stampatutto(int a[], int n, int max, int min, long int somma_v, float media
 
 int main(void)
 {
-    int array[MAX];
-    int n;
+    int array[MAX]; //vettore che riempiamo di n cifre per un massimo di MAX
+    int n; //numero di valori salvati nell'array
 
     printf("Quanti valori si vogliono usare? ");
     scanf("%d", &n);
-    while(n > MAX)
+
+    while(n > MAX) //stampa l'errore in caso vengano inseriti più di MAX valori, il controllo viene fatto ad ogni inserimento
     {
         printf("Valore massimo superato \nIl massimo di valori sono 100, quanti valori si vogliono usare? ");
         scanf("%d", &n);
     }
 
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n; i++) //ciclo for per riempire l'array
     {
         printf("Inserire il valore %d: ", i+1);
         scanf("%d", &array[i]);
@@ -51,14 +53,27 @@ int main(void)
     //richiamo media
     float media_v = media(array, n);
     //richiamo deviazione
-    int deviazione = differenza_da_media(array, n);
+    float deviazione = differenza_da_media(array, n);
 
     //stampa tutto
     stampatutto(array, n, max, min, somma_v, media_v, deviazione);
+
+    return 0;
 }
 
 int massimo(int a[], int n)
 {
+    /*
+    descrizione: 
+    questa funzione trova il massimo valore contenuto nei primi n valori dell'array
+
+    input: 
+    Array: int[] - array dei valori da cui ricavare il massimo 
+    n: int - n di valori contenuti nell'array da utilizzare
+
+    output:
+    int - valore massimo dell'array passato
+    */
     int max = a[0];
     for(int i=1; i<n; i++)
     {
@@ -70,6 +85,17 @@ int massimo(int a[], int n)
 
 int minimo(int a[], int n)
 {
+    /*
+    descrizione: 
+    questa funzione trova il minimo valore contenuto nei primi n valori dell'array
+
+    input: 
+    Array: int[] - array dei valori da cui ricavare il minimo 
+    n: int - n di valori contenuti nell'array da utilizzare
+
+    output:
+    int - valore minimo dell'array passato
+    */
     int min = a[0];
     for(int i=1; i<n; i++)
     {
@@ -81,6 +107,17 @@ int minimo(int a[], int n)
 
 long int somma(int a[], int n)
 {
+    /*
+    descrizione: 
+    questa funzione calcola la somma dei primi n valori dell'array
+
+    input: 
+    Array: int[] - array dei valori da cui ricavare la somma 
+    n: int - n di valori contenuti nell'array da utilizzare
+
+    output:
+    long int - somma dei valori
+    */
     long int sum = 0;
     for(int i=0; i<n; i++)
     {
@@ -91,19 +128,42 @@ long int somma(int a[], int n)
 
 float media(int a[], int n)
 {
-    long int sum = somma(a, n);
-    float med = sum/n;
+    /*
+    descrizione: 
+    questa funzione calcola la media dei primi n valori dell'array
+
+    input: 
+    Array: int[] - array dei valori da cui ricavare la media 
+    n: int - n di valori contenuti nell'array da utilizzare
+
+    output:
+    float - media dei valori
+    */
+    long int sum = somma(a, n); //richiama la funzione somma
+    float med = (double)sum / n; //cast necessario per calcolare la media in virgola mobile 
     return med;
 }
 
 float differenza_da_media(int a[], int n)
 {
+    /*
+    descrizione: 
+    questa funzione calcola la differenza massima tra i primi n valori dell'array e la media 
+    per rappresentare in fomrmato "media +- valore" l'array fino a n 
+
+    input: 
+    Array: int[] - array dei valori da cui ricavare la media 
+    n: int - n di valori contenuti nell'array da utilizzare
+
+    output:
+    float - media dei valori
+    */
     int max = massimo(a, n);
     int min = minimo(a,n);
     float media_v = media(a, n);
 
-    float dif_m = media_v - min;
-    float dif_M = max - media_v;
+    float dif_m = media_v - (float)min; //cast necessario per calcolare la media in virgola mobile 
+    float dif_M = (float)max - media_v; //cast necessario per calcolare la media in virgola mobile 
 
     if (dif_M > dif_m)
         return dif_M;
@@ -113,6 +173,18 @@ float differenza_da_media(int a[], int n)
 
 void stampatutto(int a[], int n, int max, int min, long int somma_v, float media_v, float diff)
 {
+    /*
+    descrizione: 
+    questa funzione calcola la differenza massima tra i primi n valori dell'array e la media 
+    per rappresentare in fomrmato "media +- valore" l'array fino a n 
+
+    input: 
+    Array: int[] - array dei valori da cui ricavare la media 
+    n: int - n di valori contenuti nell'array da utilizzare
+
+    output:
+    float - media dei valori
+    */
     printf("array: ");
     for(int i=0; i<n; i++)
     {
