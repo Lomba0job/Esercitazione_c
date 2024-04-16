3f42acee83cf4304a2d5f0c0759506a3cd7ffdd0
Michele Lombardi <michelelombardi.job@gmail.com>
2024-04-16 18:29:15 +0200

    esercizio Lista

--- liste/esercizo_lista_semplice.c
+++ liste/esercizo_lista_semplice.c
@@ -4,3 +4,12 @@
  * @author Lombardi Michele 
  */
 
+/*
+definire una lista con all'interno una int un float e una stringa
+
+scrivere le funzioni per agigungere un elemnto nella lista 
+eliminare l'ultimo elemento della lista
+
+cercare nella lista un elemento che abbia una stringa uguale a str
+cercare nella lista gli elemento con N come int 
+*/
